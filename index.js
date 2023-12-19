3409f9513c533df294b9b4ddec62c8b9d45a5977
Daniel <drienas@01MacAir1.local>
2023-12-19 14:10:55 +0100

    add better object

--- index.js
+++ index.js
@@ -14,9 +14,40 @@ app.use(bodyParser.json());
 
 app.post(`/:filiale`, (req, res) => {
   const filiale = req.params.filiale;
-  console.log(filiale);
-  console.log(req.body);
-  res.status(200).json(req.body);
+  const body = req.body;
+
+  console.log(`**Neues Event für Filiale ${filiale}**`);
+
+  if (!body.event) {
+    res.status(500).send(`Internal Server Error`);
+    console.log(body);
+    return;
+  }
+
+  try {
+    let dossier = body.data.dossier;
+
+    let data = {
+      event: body.event,
+      type: body.object.type,
+      user: body.user.name,
+      createdAt: dossier.created_at,
+      updatedAt: dossier.updated_at,
+      completedAt: dossier.completion_date_time,
+      customer: dossier.contact,
+      vehicle: dossier.vehicle,
+      tasks: dossier.workshop_tasks,
+    };
+
+    console.log(data);
+    console.log(JSON.stringify(data, null, 2));
+
+    res.status(200).send();
+  } catch (err) {
+    console.log(body);
+    console.log(`Fehler ${err}`);
+    res.status(500).send();
+  }
 });
 
 app.listen(PORT, () => {
